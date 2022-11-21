fefeace5b77cc260f4a1ea165fcba821192e3cdc
Angel_Pena <1103314@est.intec.edu.do>
2022-11-21 19:07:18 -0400

    Agregando tests

---
+++ test.py
@@ -0,0 +1,15 @@
+import fibonacci as f
+import unittest
+
+
+class ftest(unittest.TestCase):
+    def test(self):
+        self.assertEquals(f.fibonacci_of(5), 5)
+        self.assertEquals(f.fibonacci_of(6), 8)
+        self.assertEquals(f.fibonacci_of(7), 13)
+        self.assertEquals(f.fibonacci_of(8), 21)
+        self.assertEquals(f.fibonacci_of(9), 20, "Test failed")
+
+
+if __name__ == '__main__':
+    unittest.main()
