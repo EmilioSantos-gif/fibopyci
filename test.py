51de7ae6028ac2ff1825c9c0b50f258e452ee58b
Ángel Peña <70070167+Blaster2036@users.noreply.github.com>
2022-11-25 08:24:34 -0400

    Linting

--- test.py
+++ test.py
@@ -9,8 +9,7 @@ class ftest(unittest.TestCase):
         self.assertEquals(f.fibonacci_of(6), 8)
         self.assertEquals(f.fibonacci_of(7), 13)
         self.assertEquals(f.fibonacci_of(8), 21)
-        #self.assertEquals(f.fibonacci_of(9), 20, "Test failed")
-
+        
 
 if __name__ == "__main__":
     unittest.main()
